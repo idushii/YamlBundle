a9ccdec8d51f144c2dcc8d570f1236dc1b7a9c40
Nicolay I <n.ilinov@itmegastar.com>
2020-06-03 00:11:17 +0700

    Реализовал  простой вариант сборки yaml файла для локальной задачи

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dirname = path.dirname;
 
 const base = "./swagger/"
 const name = "frontend.yaml"
+const parts = "parts"
 
 
 String.prototype.replaceAll = function (search, replace) {
@@ -15,10 +16,31 @@ function loadYaml(path) {
     return fs.readFileSync(path, 'utf-8');
 }
 
-
 let allYaml = loadYaml(base + name);
+let components = []
+
+fs.readdir(base + parts, function (err, list) {
+
+    console.log(list)
+
+    list.forEach(val => components.push(loadYaml(base + parts + '/' + val).split('\r\n').map(v => `    ${v}`).join('\r\n')))
+
+    // Составить итоговый текст yaml
+    allYaml += "\r\n\r\ncomponents:\r\n\r\n  schemas:\r\n\r\n"
+    allYaml += '\r\n' + components.join('\r\n')
+
+    var listYamls = allYaml.split(/\$ref: '(.*?)'/gm)
+
+    for (let i = 0; i < listYamls.length / 2; i++) {
+        if (listYamls[i * 2 + 1])
+            listYamls[i * 2 + 1] = "$ref: '#/components/schemas/" + listYamls[i * 2 + 1].split('#/').pop() +  "'"
+    }
+
+    fs.writeFileSync('res.yaml', listYamls.join(''))
+})
 
 
+/*
 // Составляю список ссылок
 function getRefsLinks(yaml) {
     var m;
@@ -146,3 +168,4 @@ allYaml.replaceAll('-api-v1-', '-')
 fs.writeFileSync('res.yaml', allYaml)
 
 
+//*/
