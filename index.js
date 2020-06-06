d6cd2872e7f5e4e09a77ea8b6d99085627abf2b2
Nicolay I <n.ilinov@itmegastar.com>
2020-06-06 09:58:02 +0700

    Добавил аргументы для командной строки

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 let fs = require("fs")
 let path = require('path');
+const { exec } = require('child_process');
 
 const dirname = path.dirname;
 
-const base = "./swagger/"
-const name = "frontend.yaml"
-const parts = "parts"
+const args = process.argv
 
+const hasDate = args.find(r => r == '--date') || false
+
+const indexFolderOutput = args.findIndex(r => r == '-o')
+const output = indexFolderOutput != -1 ? args[indexFolderOutput + 1] : "./src/api-sdk"
+
+const indexPathFile = args.findIndex(r => r == '-i')
+const pathFile = indexPathFile != -1 ? args[indexPathFile + 1] : ""
+
+const base = path.dirname(pathFile) + "/"
+const name = path.basename(pathFile) || "swagger.yaml"
+
+const indexParts = args.findIndex(r => r == '--parts')
+const parts = indexParts != -1 ? args[indexParts + 1] : "parts"
+
+const indexTempFile = args.findIndex(r => r == '--temp')
+const tempFile = indexTempFile != -1 ? args[indexTempFile + 1] : "./temp.yaml"
 
 String.prototype.replaceAll = function (search, replace) {
     return this.split(search).join(replace);
@@ -33,8 +48,23 @@ fs.readdir(base + parts, function (err, list) {
 
     for (let i = 0; i < listYamls.length / 2; i++) {
         if (listYamls[i * 2 + 1])
-            listYamls[i * 2 + 1] = "$ref: '#/components/schemas/" + listYamls[i * 2 + 1].split('#/').pop() +  "'"
+            listYamls[i * 2 + 1] = "$ref: '#/components/schemas/" + listYamls[i * 2 + 1].split('#/').pop() + "'"
     }
 
-    fs.writeFileSync('res.yaml', listYamls.join('').split('-api-v1-').join('-'))
+    if (hasDate) {
+        listYamls = listYamls.join('').replaceAll('format: date-time', '').replaceAll('format: date', '').replaceAll('-api-v1-', '-')
+    } else {
+        listYamls = listYamls.join('')
+    }
+
+    fs.writeFileSync(tempFile, listYamls)
+
+    exec(`openapi-generator generate --input-spec ${tempFile} --generator-name typescript-fetch --output ${output} --config api.json`, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err)
+        }
+        fs.unlinkSync(tempFile)
+    });
+
+
 })
